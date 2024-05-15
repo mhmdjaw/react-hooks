e317d80fa6a7f93cf3bf9497f137a7f35258f2da
Mohamad Jawhar <mohamad.jaouhar98@gmail.com>
2024-05-15 19:17:54 -0400

    update tsup config and package.json and add peer dependencies

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['src/index.ts'],
-  format: ['cjs', 'esm'], // Build for commonJS and ESmodules
+  entry: ['src/**/*@(ts|tsx)'],
+  format: ['esm', 'cjs'], // Build for commonJS and ESmodules
   dts: true, // Generate declaration file (.d.ts)
-  sourcemap: true,
-  clean: true
+  treeshake: true, // enable treeshaking
+  sourcemap: true, // generate source map for js and mjs files
+  clean: true // clean dist directory before each build
 })
