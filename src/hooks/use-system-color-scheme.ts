b916a3b41c7c0f42fd3a92aeea925e5e42d5f060
Mohamad Jawhar <mohamad.jaouhar98@gmail.com>
2024-05-18 14:26:42 -0400

    add useSystemColorScheme

---
+++ src/hooks/use-system-color-scheme.ts
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react'
+
+const query = '(prefers-color-scheme: dark)'
+
+/**
+ * Returns current system color scheme. Updates on change.
+ * @returns "dark" or "light"
+ */
+export const useSystemColorScheme = () => {
+  const [colorScheme, setColorScheme] = useState('light')
+
+  useEffect(() => {
+    const listener = (event: MediaQueryListEvent) => {
+      setColorScheme(event.matches ? 'dark' : 'light')
+    }
+
+    window.matchMedia(query).addEventListener('change', listener)
+    return () => window.matchMedia(query).removeEventListener('change', listener)
+  })
+
+  return colorScheme
+}
