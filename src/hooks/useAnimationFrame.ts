4d0a3a40edfc179a6c82490f66bdf71f7874aba6
Mohamad Jawhar <mohamad.jaouhar98@gmail.com>
2024-05-20 19:31:26 -0400

    add useAnimationFrame

---
+++ src/hooks/useAnimationFrame.ts
@@ -0,0 +1,73 @@
+import { useCallback, useEffect, useRef } from 'react'
+
+interface CallbackParams {
+  timestamp: number
+  startTime: number
+  complete: () => void
+}
+
+/**
+ * Manages `requestAnimationFrame` and handles starting and cancelling it.
+ * @param callback Function that will be called when the next frame is available. `timestamp` of `requestAnimationFrame` and `complete` function are passed as parameters.
+ * @param {boolean} autoInvoke Determines whether the `requestAnimationFrame` should start when the component mounts. `false` by default.
+ * @param {React.DependencyList} depsList Optional list of dependencies used in the `callback` function. Pass state values that the `callback` function might depend on.
+ * @returns start and cancel functions and a function to check the status of the `requestAnimationFrame`.
+ */
+export const useAnimationFrame = (
+  callback: (callbackParams: CallbackParams) => void,
+  autoInvoke: boolean = false,
+  depsList?: React.DependencyList
+) => {
+  const requestAnimationFrameRef = useRef<number | null>(null)
+
+  const start = useCallback(
+    () => {
+      cancel()
+      let startTime: number
+
+      const runAnimation = (timestamp: number) => {
+        let complete = false
+        callback({ timestamp, startTime, complete: () => (complete = true) })
+        if (!complete) {
+          requestAnimationFrameRef.current = window.requestAnimationFrame((timestamp) => {
+            runAnimation(timestamp)
+          })
+        } else {
+          requestAnimationFrameRef.current = null
+        }
+      }
+
+      requestAnimationFrameRef.current = window.requestAnimationFrame((timestamp) => {
+        startTime = timestamp
+        runAnimation(timestamp)
+      })
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    depsList || []
+  )
+
+  const cancel = useCallback(() => {
+    if (requestAnimationFrameRef.current) {
+      window.cancelAnimationFrame(requestAnimationFrameRef.current)
+      requestAnimationFrameRef.current = null
+    }
+  }, [])
+
+  const isActive = useCallback(() => {
+    if (requestAnimationFrameRef.current) {
+      return true
+    } else {
+      return false
+    }
+  }, [])
+
+  useEffect(() => {
+    if (autoInvoke) {
+      start()
+    }
+
+    return cancel
+  }, [cancel, start, autoInvoke])
+
+  return [start, cancel, isActive] as const
+}
